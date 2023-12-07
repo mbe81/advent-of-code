552986640f0613c275b521d7ba9f8db073c6e752
Marcel van den Berg <34321907+mbe81@users.noreply.github.com>
2023-12-07 09:04:40 +0100

    Day 06

--- 2023/day06/day06.go
+++ 2023/day06/day06.go
@@ -20,10 +20,10 @@ func Run() {
 	var start time.Time
 
 	start = time.Now()
-	util.PrettyPrint(5, 1, Part1(input), start)
+	util.PrettyPrint(6, 1, Part1(input), start)
 
 	start = time.Now()
-	util.PrettyPrint(5, 2, Part2(input), start)
+	util.PrettyPrint(6, 2, Part2(input), start)
 }
 
 func Part1(input []string) int {
